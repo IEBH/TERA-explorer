64eb19139bf65644c5d51f36cc542671bdb12db6
Matt Carter <m@ttcarter.com>
2024-06-25 09:40:52 +1000

    chore: Eslint upgrade

---
+++ eslint.config.js
@@ -0,0 +1,16 @@
+import RulesMFDC from '@momsfriendlydevco/eslint-config';
+
+export default [
+	{
+		// Global ignore rules - Do not add any other keys to this object
+		ignores: [
+			'.*',
+			'docs/',
+			'dist/',
+			'node_modules/',
+			'scripts/**/*',
+			'**/.wrangler/',
+		],
+	},
+	...RulesMFDC,
+]
